eaa79abf285ce70412b0056bb2841b6fa1367daa
Thomas Mattsson <tmattsson0@gmail.com>
2019-12-04 19:07:24 +0900

    .

--- flask_washingservice.py
+++ flask_washingservice.py
@@ -4,3 +4,15 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+
+config = {
+  "apiKey": os.environ['AIzaSyBuIgw9ioawAlgSln9IW4QKnKJYPdy3TEc'],
+  "authDomain": "washing-machine-service-fdc63.firebaseapp.com",
+  "databaseURL": "https://washing-machine-service-fdc63.firebaseio.com",
+  "projectId": "washing-machine-service-fdc63",
+  "storageBucket": "washing-machine-service-fdc63.appspot.com",
+  "serviceAccount": "washing-machine-service-fdc63-firebase-adminsdk-la50h-eabce11589.json",
+  "messagingSenderId": "703001830397"
+}
