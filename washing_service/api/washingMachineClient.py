af9f55b7f97e05a30ebb28e65fa54cc169337305
Thomas Mattsson <tmattsson0@gmail.com>
2019-12-10 22:42:01 +0900

    Update washingMachineClient.py

--- washing_service/api/washingMachineClient.py
+++ washing_service/api/washingMachineClient.py
@@ -1,9 +1,12 @@
 from flask import request, jsonify
-from washing_service import app, db
 import requests
 
 
-def get_wm_status(timestamp):
-    r = requests.get("http://www.washing_machine.com/fetch", params={"start_time":timestamp})
+# Gets the status of a washing machine along with all timestamps that are newer than the timestamp in the parameter.
+def get_wm_status(id, timestamp=None):
+    if timestamp is not None:
+        r = requests.get("http://www.washing_machine.com/<id>/fetch", params={"start_time":timestamp})
+    else:
+        r = requests.get("http://www.washing_machine.com/<id>/fetch")
     return r
 
