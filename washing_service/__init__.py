5da58bb421576cf3d6b6a91a75af133063b236b6
Thomas Mattsson <tmattsson0@gmail.com>
2019-12-19 08:54:55 +0900

    Added json auth file for firebase. So TA's can run it.

--- washing_service/__init__.py
+++ washing_service/__init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 # Initialize Firestore DB
 if (not len(firebase_admin._apps)):
-    cred = credentials.Certificate('/Users/kunduzb17/Downloads/washing-machine-service-fdc63-firebase-adminsdk-la50h-24cdd573d7.json')
+    cred = credentials.Certificate('washing-machine-service-fdc63-firebase-adminsdk-la50h-eabce11589.json')
     default_app = firebase_admin.initialize_app(cred)
 
 db = firestore.client()
